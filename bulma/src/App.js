04bdf60f6eb51d3938a67aef62dead6614d5aee6
trickypr <trickypr@users.noreply.github.com>
2019-10-14 14:06:42 +1100

    (Some) bulma tests

---
+++ bulma/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Box from 'react-bulma-components/lib/components/box'
+
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <Box style={{ width: 300 }}>
+        <h2>Totally a card</h2>
+        <p>Keyboard mashing:</p>
+        <p>skdjgdfjfgsd dffgadjhg aeiot li.aesljtg aiwu tpserita;sej ldzjg ;akjwet oaweit itlkdgj lrak tj;ru guporartu akrrjflaksgj ;agja;srjtgoag;adjtgiwiJRAPT JAE AEITRHJ POAESIT APEWEIP TAIWJTR OWIT POAERITPOUwr oritO JPu tilj tpiajrt oai  roptjerakltgnep pitawe[it poaerut eraiut oparit opaerit poaerit aeroit[apwr.sdfg ear-]t wertjweoptuae o;tr au e,mfng st o;awi[-t wlktjwr;ot jepog erlrkjg ope tjpeorgj eorptu </p>
+      </Box>
+    </div>
+  );
+}
+
+export default App;
---
+++ pure-bulma/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.scss'
+
+function App() {
+  return (
+    <div className="App">
+      <div className="box" style={{ width: 300 }}>
+        <h2>Totally a card</h2>
+        <p>Keyboard mashing:</p>
+        <p>skdjgdfjfgsd dffgadjhg aeiot li.aesljtg aiwu tpserita;sej ldzjg ;akjwet oaweit itlkdgj lrak tj;ru guporartu akrrjflaksgj ;agja;srjtgoag;adjtgiwiJRAPT JAE AEITRHJ POAESIT APEWEIP TAIWJTR OWIT POAERITPOUwr oritO JPu tilj tpiajrt oai  roptjerakltgnep pitawe[it poaerut eraiut oparit opaerit poaerit aeroit[apwr.sdfg ear-]t wertjweoptuae o;tr au e,mfng st o;awi[-t wlktjwr;ot jepog erlrkjg ope tjpeorgj eorptu </p>
+      </div>
+
+      <a className="button is-primary">I am a button</a>
+
+      <input type="text" class="input" name="" id="" defaultValue="Enter keyboard spam" />
+    </div>
+  );
+}
+
+export default App;
